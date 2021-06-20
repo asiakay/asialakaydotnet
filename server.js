e236a06750924f8465d2e57fb012dc841725c153
Microsoft Learn Student <learn@contoso.com>
2021-06-19 20:51:57 -0400

    added links to icons in footer

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/', function(req, res){
   res.render('pages/index');
 });  
 app.get('/contact')
-
+res.render('pages/contact');
 // not working 
 /* app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, 'pages/index'));
